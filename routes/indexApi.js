a55830bc9fb8a946027bba364d6fe382084301c7
plusmail <plus4957@gmail.com>
2022-12-14 12:43:45 +0900

    user데이트블 usersecess 기본값 1로 수

--- routes/indexApi.js
+++ routes/indexApi.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const cookieParser = require("cookie-parser");
 const models = require("../models");
 const fs = require('fs');
+const {getPagination, getPagingData} = require("../controller/pagination");
+const {Op} = require("sequelize");
 
 
 router.get('/api/airplane/:id', async function (req, res, next) {
@@ -24,6 +26,72 @@ router.get('/api/airplane/:id', async function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+
+
+router.get('/client/userMngList/:usersecess', async (req,res,next)=>{
+    //usersecess 정상회원, 탈퇴회원 구분
+
+    const usersecess = req.params.usersecess;
+    let { searchType, keyword } = req.query;
+
+    const contentSize = Number(process.env.CONTENTSIZE); // 한페이지에 나올 개수
+    const currentPage = Number(req.query.currentPage) || 1; //현재페이
+    const { limit, offset } = getPagination(currentPage, contentSize);
+
+    keyword = keyword ? keyword : "";
+
+    let dataAll = await models.user.findAll({
+        where: {
+            [Op.and] : [
+                {
+                    usersecess: usersecess
+                }
+            ],
+            [Op.or]: [
+                {
+                    userid: { [Op.like]: "%" +keyword+ "%" }
+                },
+                {
+                    username: { [Op.like]: "%" + keyword + "%" }
+                }
+            ]
+
+        },
+        limit, offset
+    })
+
+    let dataCountAll = await models.user.findAndCountAll({
+        where: {
+            [Op.and] : [
+                {
+                    usersecess: usersecess
+                }
+            ],
+            [Op.or]: [
+                {
+                    userid: { [Op.like]: "%" +keyword+ "%" }
+                },
+                {
+                    username: { [Op.like]: "%" + keyword + "%" }
+                }
+            ]
+        },
+        limit, offset
+    })
+
+    const pagingData = getPagingData(dataCountAll, currentPage, limit);
+
+    let cri = {searchType,keyword};
+
+    let btnName = (Boolean(Number(usersecess)) ? "회원 리스트" : "탈퇴회원 조회");
+
+    console.log("usersecbtt->", btnName)
+    let Manager = {};
+    let Auth ={};
+    let list = dataAll;
+
+    res.send({cri, list, btnName, pagingData, Manager, usersecess, Auth});
+})
 
 
 module.exports = router;
